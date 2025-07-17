4b323ac709419ecafd920731cf22d2cdbd6f26a7
Lydia-meftah <73774355+Lydia-meftah@users.noreply.github.com>
2025-07-17 12:53:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ def verify_password(username, password):
         return username
 
 # ---- PIPELINE TRANSFORMERS ----
-summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
-
+summarizer = pipeline("summarization", model="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+#summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 # summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
 # ---- CHUNKING LONG TEXT ----
